fa27a84ac4f65238c2fe5437b2f7d0a923a63726
Subiksha-A <143953071+Subiksha-A@users.noreply.github.com>
2025-06-26 13:19:40 +0530

    Create d_ff.v

---
+++ DAY 3/d_ff.v
@@ -0,0 +1,20 @@
+module d_ff(
+    output reg q,
+    input d,
+    input rst_n,
+    input clk
+    );
+
+    always @(posedge clk or negedge rst_n)
+ begin
+        if (!rst_n)
+        begin
+            q <= 0;
+        end
+        else
+        begin
+            q <= d;
+        end
+    end
+  
+endmodule
