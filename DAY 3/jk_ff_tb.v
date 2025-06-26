97f3395c9ac019001b7814aa4dcbb46474b40c20
Subiksha-A <143953071+Subiksha-A@users.noreply.github.com>
2025-06-26 13:27:44 +0530

    Create jk_ff_tb.v

---
+++ DAY 3/jk_ff_tb.v
@@ -0,0 +1,25 @@
+module jk_ff_tb;
+    wire q;          
+    wire qb;       
+    reg j;           
+    reg k;           
+    reg clk;         
+
+    jk_ff uut (q, qb, j, k, clk); 
+
+    initial begin
+        j = 0;
+        k = 0;
+        clk = 0;
+        #1000 $finish;  
+    end
+    always #30 clk = ~clk;  
+    always #50 k = ~k; 
+  	always #100 j = ~j;  
+
+  initial begin
+  $dumpfile("dump.vcd");
+  $dumpvars;
+  #10000 $finish;
+  end
+endmodule
