4c35c5c6b530d93a07092af88ca0d163aac04412
Subiksha-A <143953071+Subiksha-A@users.noreply.github.com>
2025-07-05 22:27:34 +0530

    Create updowncounter_tb.v

---
+++ DAY 4/updowncounter_tb.v
@@ -0,0 +1,23 @@
+module updowncounter_testbench();
+reg clk, reset,up_down;
+wire [3:0] counter;
+
+up_down_counter dut(clk, reset,up_down, counter);
+initial begin 
+clk=0;
+forever #5 clk=~clk;
+end
+initial begin
+reset=1;
+up_down=0;
+#20;
+reset=0;
+#200;
+up_down=1;
+end
+initial begin
+  $dumpfile("dump.vcd");
+  $dumpvars;
+  #10000 $finish;
+end
+endmodule 
