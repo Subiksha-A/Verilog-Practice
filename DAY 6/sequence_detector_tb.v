729f2464647562f534e37ad710f162fc84eae435
Subiksha-A <143953071+Subiksha-A@users.noreply.github.com>
2025-07-05 22:37:35 +0530

    Create sequence_detector_tb.v

---
+++ DAY 6/sequence_detector_tb.v
@@ -0,0 +1,54 @@
+module tb_sequencedetector;
+
+  reg x;
+  reg clk;
+  reg rst;
+  wire z;
+
+  sequencedetector uut (
+    .x(x),
+    .clk(clk),
+    .rst(rst),
+    .z(z)
+  );
+
+  initial begin
+    clk = 0;
+    forever #5 clk = ~clk;
+  end
+
+  initial begin
+    $monitor("Time=%0t, state=%b, x=%b, z=%b", $time, uut.state, x, z);
+
+    rst = 1;
+    x = 1;
+    #10 rst = 0;
+
+    #10 x = 0;
+    #10 x = 1;
+    #10 x = 1;
+
+    #10 x = 1;
+    #10 x = 0;
+    #10 x = 1;
+
+    #10 x = 1;
+    #10 x = 1;
+    #10 x = 0;
+
+    #10 x = 1;
+    #10 x = 0;
+    #10 x = 1;
+
+    #10 x = 0;
+    #10 x = 1;
+    #10 x = 0;
+
+    #50 $finish;
+  end
+
+  initial begin
+    $dumpfile("dump.vcd"); $dumpvars;
+  end
+
+endmodule
